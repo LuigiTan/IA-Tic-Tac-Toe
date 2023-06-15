312d5eaf11314c7f6e2a03dd35ad07f9b39da8a7
LuigiTan <gibran.garcia@uniat.edu.mx>
2023-06-15 11:01:00 -0600

    Se añadio la funcion de winner, tiene empate y NO_ONE

--- AI-Tic-Tac-Toe.cpp
+++ AI-Tic-Tac-Toe.cpp
@@ -13,7 +13,7 @@ const char O = 'o';
 void instructions();
 char playerSymbol();
 char OponentSymbol(char Symbol);
-char Winner();
+char winner(const vector<char>& board);
 bool AskYesNo(string question);
 
 int main()
@@ -86,5 +86,36 @@ bool AskYesNo(string question)
    }
 
 }
+//Return the piece winner a tie or noBody
+char winner(const vector<char>& board)
+{
+    //This are the posibilities to win
+    const int WINNING_POS[8][3] = { {0, 1, 2}, //Horizontal
+                                    {3, 4, 5}, //Horizontal
+                                    {6, 7, 8}, //Horizontal
+                                    {0, 3, 6}, //Vertical
+                                    {1, 4, 7}, //Vertical
+                                    {2, 5, 8}, //Vertical
+                                    {2, 4, 6}, //Horizontal
+                                    {0, 4, 8} }; //Horizontal
+    const int TOTAL_ROWS = 8;
 
+    //Return the winner
+    for (int row = 0; row < TOTAL_ROWS; row++)
+    {
+        if ((board[WINNING_POS[row][0]] != EMPTY) &&
+            (board[WINNING_POS[row][0]] == board[WINNING_POS[row][1]]) &&
+            (board[WINNING_POS[row][1]] == board[WINNING_POS[row][2]]))
+        {
+            return board[WINNING_POS[row][0]];
+        }
+    }
 
+    //Return a Tie
+    if (count(board.begin(), board.end(), EMPTY) == 0) {
+        return TIE;
+    }
+
+    //Return that no one is the winner yet
+    return NO_ONE;
+}
